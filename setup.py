bf72bdf8c5c454ce8debbe8aa6e6782a295cc4b1
Divyanshu <83786310+Rimurun-Tempest@users.noreply.github.com>
2023-04-17 02:57:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     },
     scripts=[],
     install_requires=[
-        'protobuf==3.20.0',
+        'protobuf==3.20.3',
         'packaging==20.9',
         'absl-py',
         'ddsp',
